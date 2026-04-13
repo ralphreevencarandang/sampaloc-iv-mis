4ae5d74e31fc4409aff82882ae88ad1cedb631b4
ralphreevencarandang <rvncrndng@gmail.com>
2026-04-13 23:18:42 +0800

    initial commit

---
+++ app/(Resident)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section>
+      <div>
+        <h1 className="text-4xl font-bold text-center mt-10">Welcome to Sampaloc IV MIS</h1>
+        <p className="text-center mt-4 text-lg">Your one-stop solution for managing barangay data and operations.</p>
+      </div>
+    </section>
+  );
+}
